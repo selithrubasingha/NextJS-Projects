b2f72a774f01b6b6f59e83dc68c8f5990c8342c1
Selith <selithrubasingha@gmail.com>
2026-02-06 22:58:27 +0530

    CSS modules

--- nextjs-dashboard/app/page.tsx
+++ nextjs-dashboard/app/page.tsx
@@ -1,11 +1,14 @@
 import AcmeLogo from '@/app/ui/acme-logo';
 import { ArrowRightIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
+import styles from '@/app/ui/home.module.css';
 
 export default function Page() {
   return (
     <main className="flex min-h-screen flex-col p-6">
-      <div className="flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-52">
+  
+      {/* <div className="flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-52"> */}
+      <div className={styles.shape} >
         {/* <AcmeLogo /> */}
       </div>
       <div className="mt-4 flex grow flex-col gap-4 md:flex-row">
@@ -20,7 +23,7 @@ export default function Page() {
             </a>
             , brought to you by Vercel.
           </p>
-          <Link
+          <Link 
             href="/login"
             className="flex items-center gap-5 self-start rounded-lg bg-blue-500 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-blue-400 md:text-base"
           >
