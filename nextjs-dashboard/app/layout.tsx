4c86ea9b9f574f2ec2708127e5e1cbc0971be6f3
Selith <selithrubasingha@gmail.com>
2026-02-06 23:50:30 +0530

    adding new fonts

--- nextjs-dashboard/app/layout.tsx
+++ nextjs-dashboard/app/layout.tsx
@@ -1,7 +1,7 @@
 // adding the css file to this is whole lot easier than adding it to each page.
 
 import '@/app/ui/global.css';
-
+import {inter,lusitana} from '@/app/ui/fonts';
 /*
 This is the ultimate root layout , kind of like the app.tsx in vite react? 
 according to my understanding
@@ -13,7 +13,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={`${lusitana.className} antialiased`}>{children}</body>
     </html>
   );
 }
