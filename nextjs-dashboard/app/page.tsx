908c807b1afa0ebb07e440c3fe39cdc68d7c52cb
Selith <selithrubasingha@gmail.com>
2026-02-07 00:03:38 +0530

    adding super cool images (next images , not imgs)

--- nextjs-dashboard/app/page.tsx
+++ nextjs-dashboard/app/page.tsx
@@ -2,6 +2,7 @@ import AcmeLogo from '@/app/ui/acme-logo';
 import { ArrowRightIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
 import styles from '@/app/ui/home.module.css';
+import Image from 'next/image';
 
 export default function Page() {
   return (
@@ -9,7 +10,7 @@ export default function Page() {
   
       {/* <div className="flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-52"> */}
       <div className={styles.shape} >
-        {/* <AcmeLogo /> */}
+        <AcmeLogo />
       </div>
       <div className="mt-4 flex grow flex-col gap-4 md:flex-row">
         <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
@@ -32,6 +33,13 @@ export default function Page() {
         </div>
         <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
           {/* Add Hero Images Here */}
+          <Image 
+            src = "/hero-desktop.png"
+            alt = "Hero Image"
+            width={500}
+            height={300}
+            className="Screenshots of the dashboard project showing desktop version"
+          />
         </div>
       </div>
     </main>
